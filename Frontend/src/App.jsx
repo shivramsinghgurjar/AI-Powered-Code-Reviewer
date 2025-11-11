e4819e2018e58911a1e69f6ce7468a99d0a604dd
shivramsinghgurjar <shivramsinghgurjar2@gmail.com>
2025-11-11 11:09:06 +0530

    Change fontend localhost url to vercel depployed backed url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-powered-code-reviewer-eta.vercel.app/ai/get-review', { code })
     setReview(response.data)
   }
 
