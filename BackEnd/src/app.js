067285dc8f12acb3b579d10ed3f3ccd285de03d4
shivramsinghgurjar <148748849+shivramsinghgurjar@users.noreply.github.com>
2026-03-25 12:14:03 +0530

    fix: cors + performance

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,18 +1,25 @@
 const express = require('express');
-const aiRoutes = require('./routes/ai.routes')
-const cors = require('cors')
+const aiRoutes = require('./routes/ai.routes');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://ai-powered-code-reviewer-eta.vercel.app",
+        "https://ai-powered-code-reviewer-hyvu0rsud.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
-
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
-app.use('/ai', aiRoutes)
+app.use('/ai', aiRoutes);
 
-module.exports = app
+module.exports = app;
